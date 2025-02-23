2e5ba2954085e02785b018eb02971b96aefa3918
yeye9ye <yeye9ye@gmail.com>
2025-02-22 23:30:03 -0500

    Add home module into app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <main className="main">
+        <Home />
 
       </main>   
     
